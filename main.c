6d76a5775708d8b95eda7a0229bf9cece90de79e
9Strike <36662302+9Strike@users.noreply.github.com>
2018-08-30 19:36:27 +0200

    Add basic Gtk window from glade

--- main.c
+++ main.c
@@ -2,4 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+  GtkBuilder *builder;
+  GtkWidget *window;
+  gtk_init(&argc, &argv);
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+  gtk_builder_connect_signals(builder, NULL);
+  g_object_unref(builder);
+  gtk_widget_show(window);                
+  gtk_main();
+  return 0;
 }
