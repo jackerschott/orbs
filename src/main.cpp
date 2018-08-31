838dd69372569af393a80570de6fe51fe73da967
9Strike <36662302+9Strike@users.noreply.github.com>
2018-08-31 16:45:28 +0200

    Added "Create Ring" signals

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,17 @@ void on_button_render_clicked()
   g_print ("Render\n");
 }
 
+void on_button_ring_clicked()
+{
+  g_print ("Create Ring\n");
+}
+
 int main(int argc, char *argv[])
 {
   GtkBuilder *builder;
   GtkWidget *window;
   GtkWidget *button_render;
+  GtkWidget *button_ring;
   gtk_init(&argc, &argv);
   builder = gtk_builder_new();
   gtk_builder_add_from_file (builder, "gui/window_main.glade", NULL);
@@ -22,6 +28,8 @@ int main(int argc, char *argv[])
   g_signal_connect(window, "destroy", G_CALLBACK(on_window_main_destroy), NULL);
   button_render = GTK_WIDGET(gtk_builder_get_object(builder, "button_render"));
   g_signal_connect(button_render, "clicked", G_CALLBACK(on_button_render_clicked), NULL);
+  button_ring = GTK_WIDGET(gtk_builder_get_object(builder, "button_ring"));
+  g_signal_connect(button_ring, "clicked", G_CALLBACK(on_button_ring_clicked), NULL);
   gtk_widget_show(window);
   gtk_main();
   return 0;
