33f3d72e4cf555c8df84a960aba1a7696c28c899
9Strike <9Strike@users.noreply.github.com>
2018-08-31 18:15:05 +0200

    Use better shortcuts

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@ void on_window_main_destroy()
   gtk_main_quit();
 }
 
-void on_button_render_clicked()
+void on_btn_render_clicked()
 {
   g_print ("Render\n");
 }
 
-void on_button_ring_clicked()
+void on_btn_ring_clicked()
 {
   g_print ("Create Ring\n");
 }
@@ -19,17 +19,17 @@ int main(int argc, char *argv[])
 {
   GtkBuilder *builder;
   GtkWidget *window;
-  GtkWidget *button_render;
-  GtkWidget *button_ring;
+  GtkWidget *btn_render;
+  GtkWidget *btn_ring;
   gtk_init(&argc, &argv);
   builder = gtk_builder_new();
   gtk_builder_add_from_file (builder, "gui/window_main.glade", NULL);
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
   g_signal_connect(window, "destroy", G_CALLBACK(on_window_main_destroy), NULL);
-  button_render = GTK_WIDGET(gtk_builder_get_object(builder, "button_render"));
-  g_signal_connect(button_render, "clicked", G_CALLBACK(on_button_render_clicked), NULL);
-  button_ring = GTK_WIDGET(gtk_builder_get_object(builder, "button_ring"));
-  g_signal_connect(button_ring, "clicked", G_CALLBACK(on_button_ring_clicked), NULL);
+  btn_render = GTK_WIDGET(gtk_builder_get_object(builder, "btn_render"));
+  g_signal_connect(btn_render, "clicked", G_CALLBACK(on_btn_render_clicked), NULL);
+  btn_ring = GTK_WIDGET(gtk_builder_get_object(builder, "btn_ring"));
+  g_signal_connect(btn_ring, "clicked", G_CALLBACK(on_btn_ring_clicked), NULL);
   gtk_widget_show(window);
   gtk_main();
   return 0;
