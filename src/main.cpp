66bd731408baa4e5ad72a52a3e980c65081559b4
jona <jona.a99@gmail.com>
2019-04-28 20:11:45 +0200

    tmain, main: Minor restructuring.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,6 @@
 
 #include "mainwindow.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <thread>
-
-#include "tmeas.hpp"
-
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
   mainWindow windowMain; 
